70d012f8e3d90a87d05195823fbc054c41d1ef0c
omar2004712 <o.bahutair@gmail.com>
2022-07-06 13:46:59 +0400

    fixed multiple like requests bug

--- public/helpers.js
+++ public/helpers.js
@@ -27,3 +27,17 @@ const determinePostClass = (post) => {
     }
   }
 }
+
+const debounceHelper = (func, delay)=>{
+  let timeoutId;
+  //this function will take in a func and debounce it and return the new function definition
+  const debounceFunc = (...args)=>{ 
+      if(timeoutId){
+          clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(()=>{
+          func.apply(null, args);
+      }, delay);
+  }
+  return debounceFunc;
+}
--- public/likeBtn.js
+++ public/likeBtn.js
@@ -2,9 +2,10 @@ const likeButtonFunctionality = post => {
   const likeButton = post.querySelector('.like-button');
   const likeButtonCheckbox = post.querySelector(`#like-button-checkbox-${post.id}`);
   const likesCount = post.querySelector('.likes-count');
-  likeButton.addEventListener('click', async () => {
+  //used debounceHelper to prevent multiple request at the same time and reduce the stress on the server
+  likeButton.addEventListener('click', debounceHelper(async () => {
     //check if the checkbox of the like button is checked or not
-    if(!likeButtonCheckbox.checked){
+    if(!Array.from(likeButton.classList).includes('black')){
       //if not means the checkbox is being checked
       //increment the likes count
       let value = parseInt(likesCount.innerText.split(' ')[0]) + 1;
@@ -22,10 +23,10 @@ const likeButtonFunctionality = post => {
     likeButton.classList.toggle('black');
     //send an axios request to /like/postid with data of wether the user liked or not
     let url = new URL(`http://localhost:3000/like/${post.id}`)
-    await axios.post(url, { liked: !likeButtonCheckbox.checked }
+    await axios.post(url, { liked: Array.from(likeButton.classList).includes('black') }
       ,{
         'Content-Type': 'application/json'
       }
     )
-  })
+  }, 300))
 }
