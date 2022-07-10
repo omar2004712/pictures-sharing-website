b28edad188d896a361149e79432db4eace5a3478
omar2004712 <o.bahutair@gmail.com>
2022-07-10 11:18:35 +0400

    fix(deleteButton): fixed the error emitted by the delete button
    added await for the axios request before the reload of the window to
    make that the request has been handled and the post has been deleted

--- public/deleteButton.js
+++ public/deleteButton.js
@@ -1,9 +1,9 @@
 const deleteButtonFunctionality = (post) => {
   const deleteButton = post.querySelector('.delete-button');
   if(deleteButton){
-    deleteButton.addEventListener('click', () => {
+    deleteButton.addEventListener('click', async () => { //fix delete bug
       const url = new URL(`http://localhost:3000/delete/${post.id}`);
-      axios.delete(url, {
+      await axios.delete(url, {
         postId: post.id
       })
       window.location.reload();
